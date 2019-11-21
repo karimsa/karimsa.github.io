97740fe5f1f48d10dd1c118f7fba8ebf059d6fbe
Karim Alibhai <karim@alibhai.co>
2019-11-21 02:01:55 -0500

    Add: gatsby and netlify to footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
 
+import imgGatsby from '../images/gatsby.svg'
+import imgNetlify from '../images/netlify.svg'
+
 export function Footer() {
 	return (
 		<footer className="p-5">
 			<div className="container">
 				<p className="text-muted text-center">
-					Copyright &copy; {new Date().getFullYear()} Karim Alibhai. Licensed
-					under MIT license.
+					Copyright &copy; {new Date().getFullYear()} Karim Alibhai.
+				</p>
+				<p className="text-muted text-center">
+					Licensed under MIT license. Source on{' '}
+					<a href="https://github.com/karimsa/karimsa.github.io">GitHub</a>.
+				</p>
+				<p className="text-center text-muted">
+					Made with ❤️ and also{' '}
+					<img src={imgGatsby} alt="Gatsby" className="img-fluid" />. Deployed
+					with{' '}
+					<img
+						src={imgNetlify}
+						alt="Netlify"
+						className="img-fluid"
+						style={{ height: '1.5rem' }}
+					/>
+					.
 				</p>
 			</div>
 		</footer>
