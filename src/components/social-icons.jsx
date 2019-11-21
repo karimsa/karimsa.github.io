01d2653ad608250c8b77860134f7d53b58f3dbb1
Karim Alibhai <karim@alibhai.co>
2019-11-21 02:01:31 -0500

    Update: social icons to be more sexy

--- src/components/social-icons.jsx
+++ src/components/social-icons.jsx
@@ -1,50 +1,43 @@
 import React, { useState } from 'react'
 
+function SocialLink({ color, href, children, text = 'white' }) {
+	return (
+		<a
+			href={href}
+			target="_blank"
+			rel="noreferrer noopener"
+			className={`mr-2 text-${text} bg-${color} py-2 px-3 rounded-lg border-none`}
+		>
+			{children}
+		</a>
+	)
+}
+
 export function SocialIcons() {
 	const [linkedinClicked, setLinkedInClicked] = useState()
 
 	return (
 		<div>
 			<div className="text-center">
-				<a
-					href="https://github.com/karimsa"
-					target="_blank"
-					rel="noreferrer noopener"
-					className="display-4 display-md-1 mr-2"
-				>
-					<i className="fab fa-github-square" />
-				</a>
-				<a
+				<SocialLink href="https://github.com/karimsa" color="dark">
+					<i className="fab fa-github" />
+				</SocialLink>
+				<SocialLink href="https://medium.com/@karimsa" color="success">
+					<i className="fab fa-medium-m" />
+				</SocialLink>
+				<SocialLink href="https://twitter.com/@KarimSaNet" color="primary">
+					<i className="fab fa-twitter" />
+				</SocialLink>
+				<SocialLink href="mailto:karim@alibhai.co" color="warning">
+					<i className="fas fa-envelope" />
+				</SocialLink>
+				<SocialLink
 					href="mailto:karim@alibhai.co"
-					target="_blank"
-					rel="noreferrer noopener"
-					className="display-4 display-md-1 mr-2"
-				>
-					<i className="fas fa-envelope-square" />
-				</a>
-				<a
-					href="https://medium.com/@karimsa"
-					target="_blank"
-					rel="noreferrer noopener"
-					className="display-4 display-md-1 mr-2 rounded overflow-hidden"
-				>
-					<i className="fab fa-medium" />
-				</a>
-				<a
-					href="https://twitter.com/@KarimSaNet"
-					target="_blank"
-					rel="noreferrer noopener"
-					className="display-4 display-md-1 mr-2"
-				>
-					<i className="fab fa-twitter-square" />
-				</a>
-				<button
-					type="button"
-					className="bg-none border-none p-0 display-4 display-md-1 mr-2 text-muted"
 					onClick={() => setLinkedInClicked(true)}
+					color="secondary"
 				>
-					<i className="fab fa-linkedin" />
-				</button>
+					<i className="fab fa-linkedin-in" />
+				</SocialLink>
 			</div>
 			{linkedinClicked && (
 				<p className="text-muted text-center">
